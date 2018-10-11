00f0e9c1430be093298ee1c3aac686cb4b555140
ellen <ruiwanfeng@sina.com>
2018-10-11 18:58:29 +0800

    Third

--- app/src/main/java/com/example/lenovo_pc/counter/CourtCounter.java
+++ app/src/main/java/com/example/lenovo_pc/counter/CourtCounter.java
@@ -41,6 +41,36 @@ public class CourtCounter extends AppCompatActivity {
         ((TextView)findViewById(R.id.text_score1)).setText(scorea);
         ((TextView)findViewById(R.id.text_score2)).setText(scoreb);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
+    }
 
     public void add_1_3(View button){
         TextView score1=(TextView) findViewById(R.id.text_score1);
